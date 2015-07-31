7c5f2f0f1c91529673e750170e59b9722a4b12ba
Gleb Billig <gleb.billig@gmail.com>
2015-07-31 01:19:42 -0400

    Added API call to fetch IMDb rating

--- filmography.js
+++ filmography.js
@@ -5,5 +5,32 @@ filmography_section.children().each(function (index) {
 	var year_span = $(this).children("span");
 	var title =  $(this).children("b").children("a").text();
 	var year = year_span.text();
-	year_span.text(title + year);
+	
+	makeApiCall(title, year, year_span);
 })
+
+
+//http://www.omdbapi.com/?t=Maladies+&y=2012&plot=short&r=json
+
+function makeApiCall(title, year, element) {
+	var rating =  10;
+	
+	var re = new RegExp(' ', 'g');
+    apikey = title.replace(re, "+");
+	
+	year = year.replace('\xa0', '');
+	
+	var xmlHttp = new XMLHttpRequest();
+	var endpoint = 'http://www.omdbapi.com/?t=' + apikey + '&y=' + year + '&plot=short&r=json';
+
+	xmlHttp.onreadystatechange = function() { 
+		if (xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200) {
+			var data = JSON.parse(xmlHttp.responseText);
+			rating = data.imdbRating;
+			
+			element.text(rating + '\xa0' + year);
+		}
+	};
+	xmlHttp.open('GET', endpoint, true);
+	xmlHttp.send();
+}
