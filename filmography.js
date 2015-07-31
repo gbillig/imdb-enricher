e9845bea99cf0c7cbc94259475407948674ccfc1
Gleb Billig <gleb.billig@gmail.com>
2015-07-31 00:04:00 -0400

    Extracted title and year data from each film

--- filmography.js
+++ filmography.js
@@ -1,7 +1,9 @@
 console.log('Hello World');
 
 var filmography_section = $(".filmo-category-section")
-filmography_section.children().children("span").each(function (index) {
-	var year = $(this).text();
-	$(this).text("test " + year);
+filmography_section.children().each(function (index) {
+	var year_span = $(this).children("span");
+	var title =  $(this).children("b").children("a").text();
+	var year = year_span.text();
+	year_span.text(title + year);
 })
